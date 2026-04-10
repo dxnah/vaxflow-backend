76826d3e226528d918f5e86532616dbce51542da
Dinah <caburatandinah@gmail.com>
2026-04-10 17:33:00 +0800

    chore: connected backend to Supabase PostgreSQL database

--- vaxflow/settings.py
+++ vaxflow/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vaxflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'fJS4bZD0an20QOWn',
+        'HOST': 'db.cmlrzlvyijrqrzyvzzdf.supabase.co',
+        'PORT': '5432',
     }
 }
 
